d5c16bf98e19ba3b62b67ddef54be2856124a2b9
HuJub <rnysy22@gmail.com>
2020-09-18 11:46:01 +0900

    Work 9/18 - Turret
    Making a lock on system on Turret

---
+++ TD/Assets/Scripts/TurretManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretManager : MonoBehaviour
+{
+    public Transform target;
+    public Transform PartToRotate;
+    public float range = 15f;
+    public string enemyTag = "Enemy";
+    public float turnSpeed = 10f;
+   
+
+    void Start()
+    {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+    }
+
+    void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if(distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        if(nearestEnemy != null && shortestDistance <= range)
+        {
+            target = nearestEnemy.transform;
+        }
+        else
+        {
+            target = null;
+        }
+    }
+
+    void Update()
+    {
+        if(target == null)
+        {
+            return;
+        }
+
+        Vector3 dir = target.position - transform.position;
+        Quaternion lookRotate = Quaternion.LookRotation(dir);
+        Vector3 rotation = Quaternion.Lerp(PartToRotate.rotation, lookRotate, Time.deltaTime * turnSpeed).eulerAngles;
+        PartToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
+
