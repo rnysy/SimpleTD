f1af480fed88f10b6017613e400e58c98762ac22
HuJub <rnysy22@gmail.com>
2020-09-18 09:01:55 +0900

    Work 9/5~9/18
    Making Path, Spawn Enemy, PathFinding

---
+++ TD/Assets/Scripts/Enemy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 10f;
+
+    public Transform target;
+    private int wavePointIndex = 0;
+
+    void Start()
+    {
+        target = WayPoint.points[0];
+    }
+
+    void Update()
+    {
+        Vector3 direction = target.position - transform.position;
+        transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.4f)
+        {
+            GetNextWayPoint();
+        }
+    }
+
+    void GetNextWayPoint()
+    {
+        if (wavePointIndex >= WayPoint.points.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        wavePointIndex++;
+        target = WayPoint.points[wavePointIndex];
+    }
+}
