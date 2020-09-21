f708fed4380aff1688d7a3e75892372aedf1ea82
HuJub <rnysy22@gmail.com>
2020-09-21 16:23:26 +0900

    Work 9/21~
    Making CameraManager

---
+++ TD/Assets/Scripts/BuildManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    public static BuildManager instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("빌드매니저가 두 개 이상 씬에 있습니다.");
+        }
+        instance = this;
+    }
+
+    public GameObject standardTurretPrefab;
+
+    private void Start()
+    {
+        turretToBuild = standardTurretPrefab;
+    }
+
+    private GameObject turretToBuild;
+
+    public GameObject GetTurretToBuild()
+    {
+        return turretToBuild;
+    }
+}
---
+++ TD/Assets/Scripts/Tile.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public Color hoverColor;
+    public Vector3 positionOffset;
+
+    private GameObject curTurret;
+
+    private Renderer rend;
+    private Color startColor;
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+    }
+
+    private void OnMouseDown()
+    {
+        if (curTurret != null)
+        {
+            Debug.Log("그곳에는 건설할수 없습니다");
+            return;
+        }
+
+        GameObject turretTobuild = BuildManager.instance.GetTurretToBuild();
+        curTurret = (GameObject)Instantiate(turretTobuild, transform.position + positionOffset, transform.rotation);
+    }
+    private void OnMouseEnter()
+    {
+        rend.material.color = hoverColor;
+    }
+
+    private void OnMouseExit()
+    {
+        rend.material.color = startColor;
+    }
+}
